2a190e493a4b7a972c8356078299c75b2bd95bbb
ManuelaRendonP <delapava65@gmail.com>
2023-07-14 23:44:43 -0500

    New page added
    Error message and validation
    Subscribe button complete

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
 const emailInput = document.getElementById('email-input');
 const submitButton = document.getElementById('submit-button');
+const errorMessage = document.getElementById('error-message');
+const dismiss = document.getElementById('dismiss');
 
 emailInput.addEventListener('input', function () {
     if (emailInput.checkValidity()) {
         submitButton.classList.add('valid');
     } else {
         submitButton.classList.remove('valid');
+    }
+});
+
+emailInput.addEventListener('input', function () {
+    if (emailInput.validity.valid) {
+        emailInput.classList.remove('error');
+        errorMessage.textContent = '';
+        submitButton.disabled = false;
+    } else {
+        emailInput.classList.add('error');
+        errorMessage.textContent = 'Valid email required';
+        submitButton.disabled = true;
+    }
+});
+
+submitButton.addEventListener('click', function () {
+    if (emailInput.checkValidity()) {
+        window.location.href = 'subscribing.html';
+    } else {
+        submitButton.disabled = true;
     }
 });
